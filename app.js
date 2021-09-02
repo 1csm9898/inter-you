b4e0e8edd8bff10593ecefd846287100e621ce2f
choi sunmin <1csm9898@adup.kr>
2021-09-02 15:04:39 +0900

    Add : 09.02

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.render("index");
 });
@@ -12,6 +12,12 @@ app.get("/question", function (req, res) {
 });
 app.get("/calendar", function (req, res) {
   res.render("calendar");
+});
+app.get("/detail/:cate", function (req, res) {
+  const cate = req.params.cate;
+  cate === "me"
+    ? res.render("detail", { cate: "나" })
+    : res.render("detail", { cate: "우리" });
 });
 
 app.listen(3000, function () {

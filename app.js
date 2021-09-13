c7c21809a4cd8baba003fd0e01878364c3576a38
choi sunmin <1csm9898@adup.kr>
2021-09-13 18:48:16 +0900

    Add : 디비 생성 및 detail ejs와 연동

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -6,7 +7,21 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-//mongoose.connect("mongodb://localhost:27017/InterYouDB");
+mongoose.connect(
+  "mongodb+srv://" +
+    process.env.mongoDBUser +
+    ":" +
+    process.env.mongoDBKey +
+    "@cluster0.ujuvn.mongodb.net/InteryouDB"
+);
+const questionSchema = new mongoose.Schema({
+  part: String,
+  number: Number,
+  title: String,
+  content: Object,
+});
+const Question = mongoose.model("Question", questionSchema);
+
 app.get("/", function (req, res) {
   res.render("index");
 });
@@ -16,11 +31,20 @@ app.get("/question", function (req, res) {
 app.get("/calendar", function (req, res) {
   res.render("calendar");
 });
-app.get("/detail/:cate", function (req, res) {
-  const cate = req.params.cate;
-  cate === "me"
-    ? res.render("detail", { cate: "나" })
-    : res.render("detail", { cate: "우리" });
+app.get("/detail/:part", function (req, res) {
+  const part = req.params.part;
+  console.log(part);
+  console.log(typeof part);
+  Question.find({ part: part }, function (err, questions) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("detail", {
+        part: part === "me" ? "나" : "우리",
+        questions: questions,
+      });
+    }
+  });
 });
 
 app.listen(3000, function () {
